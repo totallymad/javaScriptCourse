fad3c094f8e056e883b53b378c6b55f8c10b08a9
Ilya <example@gmail.com>
2024-10-16 22:20:57 +0300

    timer 1

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -478,6 +478,44 @@ console.log(huge + ' is REALLY big!!!');
 console.log(10n / 3n);
 console.log(10 / 3); */
 
+// 08 Creating Dates NOTE 
+
+// Create a date
+
+/* const now = new Date();
+console.log(now);
+
+console.log(new Date('Aug 02 2020 18:05:41'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future); */
+
 //010 Operations With Dates NOTE
 /* const future = new Date(2037, 10, 19, 15, 23);
 console.log(Number(future));
@@ -508,7 +546,7 @@ console.log('Russia:', new Intl.NumberFormat('ru-RU', options).format(num));
 console.log('Browser:', new Intl.NumberFormat(navigator.language, options).format(num)); */
 
 //013 Timers setTimeout and setInterval NOTE
-
+/* 
 //setTimeout
 const ingrindients = ['olives', 'shpinach'];
 const pizzaTimer = setTimeout((ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
@@ -527,3 +565,4 @@ setInterval(() => {
   const seconds = now.getSeconds();
   // console.log(`Сейчас ${hour}:${minute}:${seconds}`);
 }, 1000);
+ */

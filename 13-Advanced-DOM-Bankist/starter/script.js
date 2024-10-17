0ebf949ea68a220e4fc83f387c86301a137a7479
Ilya <example@gmail.com>
2024-10-17 22:06:41 +0300

    Types of Events and Event Handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,9 +30,47 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//007 Implementing Smooth Scrolling NOTE
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.scrollX, window.scrollY);
+
+  console.log('heigh/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // Scrolling
+  // OLDSCHOOL Way
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX, 
+  //   s1coords.top + window.scrollY
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX, 
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth'
+  // });
+
+
+  // NEWSCHOOL Way
+  section1.scrollIntoView({ behavior: 'smooth' });
+})
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+// LECTURES
 
 //005 Selecting, Creating, and Deleting Elements NOTE
-
+/* 
 // Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -69,4 +107,76 @@ document.querySelector('.btn--close--cookie').addEventListener('click', function
 
   //OLD
   // message.parentElement.removeChild(message);
-})
+}) */
+
+//006 Styles, Attributes and Classes NOTE
+/* 
+//Styles 
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+// console.log(message.style.hieght);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo'
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes 
+logo.classList.add('c', 'j');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use
+// logo.className = 'ilya' */
+
+//008 Types of Events and Event Handlers NOTE
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+}
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => {
+  h1.removeEventListener('mouseenter', alertH1);
+
+}, 3000);
+
+
+// oldschool
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
+
+
+
